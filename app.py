91f42724c8912e636997441c2b51f0b8f79b7042
Marcos Antônio <moura.antonio@academico.ifpb.edu.br>
2026-06-04 15:45:40 -0300

    feat: adiciona seção contato, nesse processo foi feito: atualização do app.py, possibilitando a mudança de cor quando se clica na aba que quer navegar; foi criado o contato.html e contato.css,  atualizações pesquenas feitas no base.html que possibilitam a mudança de cor das abas do menu.

--- app.py
+++ app.py
@@ -3,11 +3,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', pagina_ativa='home')
 
 @app.route('/base')
 def base():
-    return render_template('base.html')
+    return render_template('base.html', pagina_ativa='base')
+
+@app.route('/contato')
+def contato():
+    return render_template('contato.html', pagina_ativa='contato')
 
 if __name__ == '__main__':
     app.run(debug=True)
