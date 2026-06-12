da8022de0c3ab0fa4ecde021e9a6b3cbb0a9e1ce
Gustavo Morais <gustavombdn@gmail.com>
2026-06-12 10:26:56 -0300

    feat: adiciona funções de ler, validar e salvar agendamentos no CSV e integra à rota

--- routes/scheduling.py
+++ routes/scheduling.py
@@ -1,7 +1,65 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for
 
 scheduling_bp = Blueprint('scheduling', __name__)
 
-@scheduling_bp.route('/agendamento')
+CSV_PATH = "data/agendamentos.csv"
+
+def ler_agendamentos():
+    linhas = []
+    try:
+        with open(CSV_PATH, 'r') as arquivo:
+
+            linhas = arquivo.readlines()
+            linhas_limpas = [linha.strip() for linha in linhas]
+            
+            return linhas_limpas
+        
+    except FileNotFoundError:
+        return []
+
+def validar_agendamento(data, barbeiro, horario, agendamentos_salvos):
+    for linha in agendamentos_salvos:
+        dados_linha = linha.split(',')
+        
+        if len(dados_linha) >= 4:
+            data_salva = dados_linha[1]
+            barbeiro_salvo = dados_linha[2]
+            horario_salvo = dados_linha[3]
+
+            if barbeiro_salvo == barbeiro and data_salva == data and horario_salvo == horario:
+                return False, "Este barbeiro já possui um agendamento neste dia e horário."
+            elif False:
+                pass
+            else:
+                pass
+
+    return True, "Horário disponível!"
+
+def salvar_agendamento(servico, data, barbeiro, horario, nome, telefone):
+    novo_agendamento = f"{servico},{data},{barbeiro},{horario},{nome},{telefone}\n"
+    
+    with open(CSV_PATH, mode='a') as arquivo:
+        arquivo.write(novo_agendamento)
+
+@scheduling_bp.route('/agendar', methods=['GET', 'POST'])
 def agendamento():
+    if request.method == 'POST':
+        
+        servico = request.form.get('servico')
+        data = request.form.get('data')
+        barbeiro = request.form.get('barbeiro')
+        horario = request.form.get('horario')
+        nome = request.form.get('nome_cliente')
+        telefone = request.form.get('telefone_cliente')
+        
+        agendamentos_salvos = ler_agendamentos()
+        
+        valido, mensagem = validar_agendamento(data, barbeiro, horario, agendamentos_salvos)
+        
+        if valido:
+            salvar_agendamento(servico, data, barbeiro, horario, nome, telefone)
+            return redirect(url_for('main.home'))
+        else:
+            return render_template('agendamento.html', erro=mensagem)
+        
     return render_template('agendamento.html')
