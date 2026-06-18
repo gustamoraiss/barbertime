2905e313788dea50ca203e7f1534c568dc3e2fe8
Gustavo Morais <gustavombdn@gmail.com>
2026-06-18 10:47:20 -0300

    feat: importa BackgroundScheduler ao app.py e configura o scheduler para rodar em segundo plano

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from apscheduler.schedulers.background import BackgroundScheduler
 from routes.main import main_bp
 from routes.scheduling import scheduling_bp
 from routes.contact import contact_bp
@@ -8,6 +9,15 @@ app = Flask(__name__)
 app.register_blueprint(main_bp)
 app.register_blueprint(scheduling_bp)
 app.register_blueprint(contact_bp)
+
+def rotina_temporaria():
+    pass
+
+scheduler = BackgroundScheduler()
+
+scheduler.add_job(rotina_temporaria, 'interval', minutes=15, id='limpeza_csv_job')
+
+scheduler.start()
 
 if __name__ == '__main__':
     app.run(debug=True)
