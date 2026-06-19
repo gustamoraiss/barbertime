79f646006f6cf84186b7b6fe2ce118526254ff5b
Marcos Antônio <moura.antonio@academico.ifpb.edu.br>
2026-06-19 00:05:17 -0300

    feat(style): integrar modais de erro customizados, aplicar novalidate e registrar rotas do dashboard no app.py

--- app.py
+++ app.py
@@ -2,12 +2,15 @@ from flask import Flask
 from routes.main import main_bp
 from routes.scheduling import scheduling_bp
 from routes.contact import contact_bp
+from routes.dashboard import dashboard_bp
 
 app = Flask(__name__)
+app.secret_key = 'sua_chave_secreta_aqui'
 
 app.register_blueprint(main_bp)
 app.register_blueprint(scheduling_bp)
 app.register_blueprint(contact_bp)
+app.register_blueprint(dashboard_bp)
 
 if __name__ == '__main__':
     app.run(debug=True)
